c3f5d5174b958a6e528c873d2b681e99bb8fed4a
Aaron Greenwald <aaron@aarongreenwald.com>
2015-03-25 13:39:18 -0400

    acBlockUi should support options for HTTP method
    The directive should allow users to specify specific HTTP methods
    on which to block and others to ignore.

--- ACASLibraries.Web/acas/ui/block-ui/acBlockUiManager.Service.js
+++ ACASLibraries.Web/acas/ui/block-ui/acBlockUiManager.Service.js
@@ -5,8 +5,12 @@
 			var blocks = []
 
 			var api = {
-				start: function (url) {
-					_.each(_.filter(blocks, function (x) { return url.match(new RegExp(x.urlPattern)) }), function (block) {
+				start: function (url, method) {
+					_.each(_.filter(blocks, function (x) {
+						return url.match(new RegExp(x.urlPattern))
+							&& x.excludeHttpMethods.indexOf(method) === -1
+							&& (x.httpMethods.length === 0 || x.httpMethods.indexOf(method) !== -1)
+					}), function (block) {
 						block.blocking = true
 					})
 				},
@@ -17,11 +21,11 @@
 					})
 				},
 
-
-
 				register: function (data) {
 					var newBlock = {
 						urlPattern: data.urlPattern,
+						httpMethods: data.httpMethods,
+						excludeHttpMethods : data.excludeHttpMethods,
 						blocking: data.blocking
 					}
 					blocks.push(newBlock)
@@ -36,8 +40,8 @@
 	acas.ui.angular.config(['$httpProvider', '$provide', function ($httpProvider, $provide) {
 		$provide.factory('acBlockUi-httpInterceptor', ['$q', 'acBlockUiManager', function ($q, acBlockUIManager) {
 			return {
-				'request': function (request) {
-					acBlockUIManager.start(request.url)
+				'request': function (request) {					
+					acBlockUIManager.start(request.url, request.method)
 					return request
 				},
 				'response': function (response) {
